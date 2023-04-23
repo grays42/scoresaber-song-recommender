26f905a87bf6245a5b2758dcc4edee22ab615ee9
grays42 <grayswandir@gmail.com>
2023-04-22 19:03:35 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -2,10 +2,13 @@ require 'sinatra'
 require 'httparty'
 require 'json'
 
+set :public_folder, 'public'
+set :views, 'views'
+
 get '/' do
   erb :index
 end
 
 post '/' do
-  # Your code here
+
 end
