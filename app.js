11cada278bcca879dbf39f8955fd1abcb4fd78dc
grays42 <grayswandir@gmail.com>
2023-04-22 19:42:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,42 +16,9 @@ form.addEventListener('submit', async (event) => {
   //Fetch player rank
   logMessage("Fetching player details...")
 
-  const response = await fetch(`https://scoresaber.com/api/player/${userId}/basic`)
-  const data = await response.json();
+  const basic_response = await fetch(`https://scoresaber.com/api/player/${userId}/basic`)
+  const basic_data = await basic_response.json();
 
   logMessage(`Player: ${data.name}`)
   logMessage(`Rank: ${data.rank}`)
 
-
-  // Call ScoreSaber API using userId
-  const response = await fetch(`https://new.scoresaber.com/api/player/${userId}/scores/top/50`);
-  const data = await response.json();
-
-
-  // Extract top 50 scores from response
-  const top50 = data.scores.slice(0, 50);
-
-  // Display top 50 scores on page
-  const table = document.createElement('table');
-  table.innerHTML = `
-    <thead>
-      <tr>
-        <th>Song</th>
-        <th>Mapper</th>
-        <th>Difficulty</th>
-        <th>PP</th>
-      </tr>
-    </thead>
-    <tbody>
-      ${top50.map(score => `
-        <tr>
-          <td>${score.songName}</td>
-          <td>${score.levelAuthorName}</td>
-          <td>${score.difficultyRaw}</td>
-          <td>${score.pp}</td>
-        </tr>
-      `).join('')}
-    </tbody>
-  `;
-  document.body.appendChild(table);
-});
